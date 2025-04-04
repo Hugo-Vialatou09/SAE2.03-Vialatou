edf04f85a8291e477da02038c6718a6ca9756c18
Hugo-Vialatou09 <hugo.vialatou@etu.unilim.fr>
2025-04-04 10:28:25 +0200

    ittération 1

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -2,7 +2,7 @@ let HOST_URL = 'https://mmi.unilim.fr/~vialatou2/SAE2.03-Vialatou';
 let DataMovie = {};
 
 DataMovie.requestMovies = async function () {
-    let answer = await fetch(HOST_URL + 'server/script.php?todo=getAllMovies');
+    let answer = await fetch(HOST_URL + '/server/script.php?todo=getAllMovies');
     let movies = await answer.json();
     return movies;
 };
