9f0a170351800baf96b913777d1bb064d0fc5181
Hugo-Vialatou09 <hugo.vialatou@etu.unilim.fr>
2025-04-04 10:12:34 +0200

    ittération 1

---
+++ app/component/Movie/script.js
@@ -0,0 +1,16 @@
+let templateFile = await fetch('.component/Movie/template.html');
+let template = await templateFile.text();
+
+let Movie = {};
+Movie.format = function (movies) {
+let html = "";
+movies.forEach((movie) => {
+    let movieHtml = template;
+    movieHtml = movieHtml.replace("{{titre}}", movie.name);
+    movieHtml = movieHtml.replace("{{image}}", movie.image);
+    movieHtml = movieHtml.replace("{{image1}}", movie.image);
+    html += movieHtml;
+});
+return html;
+};
+export {Movie};
---
+++ app/data/dataMovie.js
@@ -0,0 +1,10 @@
+let HOST_URL = 'https://mmi.unilim.fr/~vialatou2/SAE2.03-Vialatou/';
+let DataMovie = {};
+
+DataMovie.requestMovies = async function () {
+    let answer = await fetch(HOST_URL + 'server/script.php?todo=getAllMovies');
+    let movies = await answer.json();
+    return movies;
+};
+
+export {DataMovie};
