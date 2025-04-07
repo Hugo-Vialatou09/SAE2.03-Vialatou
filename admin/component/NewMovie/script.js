a2df47bf898c33cf9b30b694dd8a604ddb4b9a17
Hugo-Vialatou09 <hugo.vialatou@etu.unilim.fr>
2025-04-07 09:32:21 +0200

    Ittération 2

--- admin/component/NewMovie/script.js
+++ admin/component/NewMovie/script.js
@@ -2,13 +2,13 @@ let templateFile = await fetch('./component/NewMovie/template.html');
 let template = await templateFile.text();
 
 
-let NewMovieForm = {};
+let NewMovie = {};
 
-NewMovieForm.format = function(handler){
+NewMovie.format = function(handler){
     let html= template;
     html = html.replace('{{handler}}', handler);
     return html;
 }
 
 
-export {NewMovie};
+export { NewMovie };
